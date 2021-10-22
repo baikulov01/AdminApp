35f081f3213ca8abb398983d4483b8e315f2f6d5
baikulov01 <79966062+baikulov01@users.noreply.github.com>
2021-10-22 13:41:01 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
-
-"D:\Programm Files\Visual Studio 2019\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\AdminApp\AnimalTest\bin\Debug\AnimalTest.dll"
