4659035b3db63b852746e3c48fc7469210b695ab
baikulov01 <79966062+baikulov01@users.noreply.github.com>
2021-10-22 12:10:03 +0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-"D:\Programm Files\Visual Studio 2019\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\AdminApp\AnimalTest\bin\Debug\AnimalTest.dll"
+"D:\Programm Files\Visual Studio 2019\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" AnimalTest\bin\Debug\AnimalTest.dll
