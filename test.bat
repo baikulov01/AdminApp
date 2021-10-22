4cd60b86ade614b8ae4ace14ea098b0950b6b930
baikulov01 <baikulov01@inbox.ru>
2021-10-22 08:39:01 +0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
-cd "C:\Users\пк1\source\repos\AdminApp\AnimalTest\bin\Debug"
+cd AdminApp\AnimalTest\bin\Debug
 "D:\Programm Files\Visual Studio 2019\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" AnimalTest.dll
