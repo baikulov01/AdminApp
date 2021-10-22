4105716bbee9cffb64912d8e75ede6d38906db07
baikulov01 <79966062+baikulov01@users.noreply.github.com>
2021-10-22 19:41:35 +0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-"D:\Programm Files\Visual Studio 2019\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "AnimalTest\bin\Debug\AnimalTest.dll"
+"D:\Programm Files\Visual Studio 2019\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /UseVsixExtensions:true /EnableCodeCoverage "AnimalTest\bin\Debug\AnimalTest.dll"
